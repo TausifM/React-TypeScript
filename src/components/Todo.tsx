4c64b946c26a8c0b708929791e3a079a74b6faa4
TausifM <nisargcontact2020@gmail.com>
2022-08-10 12:10:35 +0530

    Todo uncontrolled component

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -3,20 +3,31 @@ import React from 'react'
 const Todo:React.FC = () => {
     const [todo, setTodo] = React.useState<string>('');
     const [todos, setTodos] = React.useState<string[]>([]);
+    const inputRef = React.useRef<HTMLInputElement | null>(null);
 
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setTodo(event.target.value);
-    }
+    // const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    //     setTodo(event.target.value);
+    // }
 
     const handleTodo = () => {
-        setTodos([...todos, todo]);
-        setTodo('');
+        // Uncontrolled component
+        if(inputRef.current) {
+            const todo = inputRef.current.value;
+            setTodos([...todos, todo]);
+            setTodo('');
+        }
+        // Controlled component
+        // setTodos([...todos, todo]);
+        // setTodo('');
     }
 
   return (
     <div>
         Todo
-        <input type="test" value={todo} onChange={(event)=>handleChange(event)}/>
+        <input type="test" value={todo} 
+        ref={inputRef}
+        // onChange={(event)=>handleChange(event)}
+        />
         <button  onClick={handleTodo}>Add</button>
         <ul>
             {todos.map((todo, index) => (
